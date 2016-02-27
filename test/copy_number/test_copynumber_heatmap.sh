8aea811bc0a3a39074076c9db46b09deab90f7f4
Ino de Bruijn <ino@ino.pm>
2016-02-26 21:02:06 -0500

    Add recurrent facets copy number plot
    - Add copynumber heatmap from facets geneCN file
    - Add simple test, create heatmap->compare

---
+++ test/copy_number/test_copynumber_heatmap.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source ${DIR}/../test_functions.sh
+
+mkdir -p ${DIR}/output
+Rscript ${DIR/test\//}/facetsGeneCNPlot.R ${DIR}/test_data/geneCN.txt ${DIR}/output/copynumber_heatmap.pdf
+compare_files ${DIR}/output/copynumber_heatmap.pdf ${DIR}/test_data/output/copynumber_heatmap.pdf
